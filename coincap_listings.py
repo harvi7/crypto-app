918ec0aa90ee0e699eeb17bb1531be1a73eb2fae
harvi7 <iamharshvirani7@gmail.com>
2020-03-05 11:26:47 -0700

    First commit

---
+++ coincap_listings.py
@@ -0,0 +1,17 @@
+import requests
+import json
+
+listings_url = 'https://api.coinmarketcap.com/v2/listings/'
+
+request = requests.get(listings_url)
+results = request.json()
+
+#print(json.dumps(results, sort_keys=True, indent=4))
+
+data = results['data']
+
+for currency in data:
+    rank = currency['id']
+    name = currency['name']
+    symbol = currency['symbol']
+    print(str(rank) + ': ' + name + ' (' + symbol + ')')
